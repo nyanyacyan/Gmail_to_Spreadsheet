69d7e2b89660d9cbe4005ec2fbc9c79e50011beb
nyanyacyan <nyanyacyan@gmail.com>
2025-04-08 06:08:24 +0900

    no message

--- src/method/gmail.py
+++ src/method/gmail.py
@@ -4,7 +4,7 @@
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # import
-import sys, os
+import sys, os, base64
 import gspread
 from googleapiclient.discovery import Resource
 from google.oauth2.service_account import Credentials
@@ -83,7 +83,7 @@ class Gmail:
 
     def _get_search_mail(self, gmail_service: Resource, **query_params: Any):
         """
-        対象のメールを検索して取得する（メールIDの取得）
+        対象のメールを検索して取得する（メールIDリストの取得）
         :param gmail_service: Gmailのサービスオブジェクト
         :param query_params: 検索条件
             subject: 件名
@@ -92,28 +92,43 @@ class Gmail:
             before: 取得するメールの最初日付
             has_attachment: 添付ファイルがあるメールを取得するかどうか
 
-        :query_params渡し方例
-            query_params = {
-                'subject': '申込み',
-                'after': '2025/01/01',
-                'before': '2025/03/01'
-            }
+        :query_params渡し方例 query_params = { 'subject': '申込み', 'after': '2025/01/01', 'before': '2025/03/01' }
         """
 
         self.logger.debug(f'query_params: {query_params}')
         query = self._build_query(**query_params)
-        mail_id_list = gmail_service.users().messages().list(userId='me', q=query).execute()
-        self.logger.debug(f'results: {mail_id_list}')
-        mail_list = mail_id_list.get('messages', [])
-        self.logger.debug(f'mail_list: {mail_list}')
-
-        if not mail_list:
+        mail_box = gmail_service.users().messages().list(userId='me', q=query).execute()
+        mail_ids_list = mail_box.get('messages', [])
+        if not mail_ids_list:
             self.logger.info('対象のメールはありませんでした。')
             return []
 
         else:
-            self.logger.debug(f'対象のメールを {len(mail_list)} 件、取得しました。')
-            return mail_list
+            self.logger.debug(f'対象のメールを {len(mail_ids_list)} 件、取得しました。')
+
+            mail_id_list = []
+            for mail_id_dict in mail_ids_list:
+                mail_id = mail_id_dict['id']
+                self.logger.debug(f'mail_id: {mail_id}')
+                mail_id_list.append(mail_id)
+            self.logger.debug(f'mail_id_list: {mail_id_list}')
+            return mail_id_list
+
+    # ----------------------------------------------------------------------------------
+
+    def _get_mail_contents(self, gmail_service: Resource, mail_id: str):
+        """
+        メールの中身を取得する
+        :param gmail_service: Gmailのサービスオブジェクト
+        :param mail_id: メールID
+        """
+        try:
+            mail_contents = gmail_service.users().messages().get(userId='me', id=mail_id).execute()
+            self.logger.debug(f'mail_contents: {mail_contents}')
+            return mail_contents
+
+        except Exception as e:
+            self.logger.error(f'メールの取得に失敗しました。: {e}')
 
     # ----------------------------------------------------------------------------------
     # クエリを作成する
@@ -135,8 +150,27 @@ class Gmail:
         return query
 
     # ----------------------------------------------------------------------------------
-    # メールの中身をデバッグする
+    # メールの本文を取得する
 
+    def _get_mail_body(self, mail_contents: Dict[str, Any]):
+        """
+        メールの本文を取得する
+        :param mail_contents: メールの中身
+        :return: メールの本文
+        """
+        try:
+            if 'data' in mail_contents['payload']['parts'][0]['body']:  # メールの本文がある場合
+                data = mail_contents['payload']['parts'][0]['body']['data']  # base64でエンコードされた本文
+                body = base64.urlsafe_b64decode(data).decode('utf-8')  # base64でデコードしてutf-8に変換
+                self.logger.debug(f'body: {body}')
+                return body
+            else:
+                self.logger.error('メールの本文が取得できませんでした。')
+
+        except Exception as e:
+            self.logger.error(f'メールの本文の取得に失敗しました。: {e}')
+
+    # ----------------------------------------------------------------------------------
     # メール件名からメールを取得する
 
 
